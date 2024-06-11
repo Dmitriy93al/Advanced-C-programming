f972d776650e3210e4171229483c3e307e7266a0
Dmitiy Alekseev <dmitriy.dog93@mail.ru>
2024-06-11 23:58:19 +0300

    Homework No. 6 for the lesson STL. Part 2

---
+++ 6/6.1/6.1/6.1.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<vector>
+#include <algorithm>
+
+int main()
+{
+	std::cout << "[IN]: ";
+
+	std::string new_string = { "1 1 2 5 6 1 2 4" };
+
+	std::vector<int>vec_eraze_duplicates;
+	std::cout << new_string << std::endl;
+
+
+	for (int i = 0; i < new_string.length(); i++) {
+		if (new_string[i] != ' ') {
+			int value = new_string[i] - '0';
+			vec_eraze_duplicates.push_back(value);
+		}
+	}
+
+	std::sort(begin(vec_eraze_duplicates), end(vec_eraze_duplicates));
+	vec_eraze_duplicates.erase(std::unique(begin(vec_eraze_duplicates), end(vec_eraze_duplicates)), end(vec_eraze_duplicates));
+
+	std::cout << "[OUT]: ";
+
+	for (auto itr = vec_eraze_duplicates.begin(); itr != vec_eraze_duplicates.end(); ++itr) {
+		{
+			std::cout << *itr << " ";
+		}
+	}
+
+	return 0;
+}
---
+++ 6/6.2/6.2/6.2.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<vector>
+#include<list>
+#include<set>
+
+template<class container>
+void print_container(container begin, container end) {
+	for (auto itr = begin; itr != end; itr++) {
+		std::cout << *itr << ", ";
+	}
+}
+
+int main()
+{
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set.begin(), test_set.end());
+
+	std::cout << "\n\n";
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list.begin(), test_list.end());
+
+	std::cout << "\n\n";
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector.begin(), test_vector.end());
+
+	std::cout << "\n";
+
+	return 0;
+}
